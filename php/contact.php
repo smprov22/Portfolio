11c271fc31dd69d1121b2899494a8b6c13313f81
smprov22 <smprovence22@gmail.com>
2020-08-27 21:46:43 -0700

    error message

--- php/contact.php
+++ php/contact.php
@@ -24,4 +24,38 @@
 	mail($to, $email_subject, $email_body, $headers);
 
 	header("Location: ../index.html");
-?>
+
+// check sender information
+	$pattern = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^";
+	if(!preg_match_all($pattern, $email, $out)) {
+		$error = $invalid_email; // for invalid email
+	}
+	if(!$email) {
+		$error = $invalid_email; // for empty email field
+	}	
+	if(!$message) {
+		$error = $invalid_message; // for empty message field
+	}
+	if (!$name) {
+		$error = $invalid_name; // for empty name field
+	}
+
+    // email header
+
+	$headers = "From: ".$name." <".$email.">\r\nReply-To: ".$email."";
+
+	if (!$error){
+
+		// sending email
+		$sent = mail($to_email,$subject,$message,$headers); 
+
+		if ($sent) {
+				// if message sent successfully
+				echo "SEND"; 
+			} else {
+				// error message
+				echo $sending_error; 
+			}
+	} else {
+		echo $error; // error message
+	}
