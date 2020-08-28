0e70489575d4985296e32042ac65f0f81e28ea83
smprov22 <smprovence22@gmail.com>
2020-08-27 22:21:34 -0700

    contact test

--- php/contact.php
+++ php/contact.php
@@ -17,14 +17,6 @@
 
 	$to = "smprovence22@gmail.com";
 
-	$headers = "From: $email_from \r\n";
-
-	$headers = "Reply-To: $email \r\n";
-
-	mail($to, $email_subject, $email_body, $headers);
-
-	header("Location: ../index.html");
-
 // check sender information
 	$pattern = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^";
 	if(!preg_match_all($pattern, $email, $out)) {
@@ -42,12 +34,15 @@
 
     // email header
 
-	$headers = "From: ".$name." <".$email.">\r\nReply-To: ".$email."";
-
 	if (!$error){
 
+		$headers = "From: $email_from \r\n";
+
+		$headers = "Reply-To: $email \r\n";
+
+		// header("Location: ../index.html");
 		// sending email
-		$sent = mail($to,$subject,$message,$headers); 
+		$sent = mail($to, $email_subject, $email_body, $headers); 
 
 		if ($sent) {
 				// if message sent successfully
